83e7ae5fdfc2ad1653a681ae9b80b4d882dc8226
Dubaille <nathan.dubaille@gmail.com>
2023-11-22 11:31:35 +0100

    dur js

--- javascript/Exo8-9/main.js
+++ javascript/Exo8-9/main.js
@@ -1,8 +1,28 @@
-//EXO6:
+//EXO8:
+/*
+function ModifText(letext){
+    alert("Je vais modifier la div");
+    var Div = document.getElementById("DivNum1");
+    Div.innerHTML = letext;
+}
 
-demande = prompt("Saisissez la valeur de N: ");
+ModifText("Hello");
+*/
+
+//EXO9:
+
+function ModifText(letext){
+    Div.innerHTML = letext;
+    alert("Je vais modifier les divs");
+    var Div = document.getElementsByName("lesdiv");
+    console.log(Div[3].letext); //displays "INPUT"
+}
+
+ModifText("Hello");
+
+/*demande = prompt("Saisissez la valeur de N: ");
 calcul = 0
 while(demande > calcul){
     calcul = calcul + 1
 }
-alert("le resultat est : " +  calcul )
+alert("le resultat est : " +  calcul )*/
