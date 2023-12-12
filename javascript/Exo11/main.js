cd95a5a6aa19fdc2506cb5f92158a5102efcadcd
Dubaille <nathan.dubaille@gmail.com>
2023-12-12 17:00:07 +0100

    ajout 12/12/2023

--- javascript/Exo11/main.js
+++ javascript/Exo11/main.js
@@ -68,12 +68,12 @@ MaDiv.addEventListener("click", test);
 
 function test(evenement) {
     fetch(
-        "http://192.168.65.64/ne_pas_enlever/ex11.json",
+        "http://192.168.64.243/main.json",
         {method : 'GET'}
         ).then(
             response => response.json()
         ).then(
-            response2 =>evenement.target.innerHTML = JSON.stringify(response2)
+            response2 => evenement.target.innerHTML = JSON.stringify(response2)
         ).catch(error => console.log("erreur : "+ error));
 }scroll
 
